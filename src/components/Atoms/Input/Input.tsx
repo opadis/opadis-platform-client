e26798f3b296347fac6079f2e9b135368c754eaf
Adrián Santos <a23311062@uthermosillo.edu.mx>
2025-07-21 15:30:25 -0700

    feat(input): creation accessible Input component

---
+++ src/components/Atoms/Input/Input.tsx
@@ -0,0 +1,41 @@
+import { cn } from "../../../utils/cn";
+import styles from "./Input.module.css";
+
+export interface InputProps{
+    label?: string
+    type?: 'text' | 'password' | 'email'
+    placeholder: string
+    id: string
+    hasError?: boolean
+    className?: string
+    errorMessage?: string
+}
+
+export function Input({
+    label, 
+    id, 
+    placeholder, 
+    type, 
+    hasError, 
+    className, 
+    errorMessage, 
+    ...props}: InputProps) {
+    return (
+        <div className={styles.containerInput}>
+            <label 
+            className={styles.label} 
+            htmlFor={id}>
+                {label}  
+            </label>
+            
+            <input  
+            className={cn(styles.input, hasError && styles.error, className)} 
+            id={id}
+            type={type ?? 'text'} 
+            placeholder={placeholder} 
+            {...props} />
+
+            {hasError && <p className={cn(styles['error-message'])}>{errorMessage}</p>}
+        </div>
+    )
+}
