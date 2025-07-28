db835e9d10282a7b845a34d1e0eb1175d6ca0432
Adriana <jeadtri05@gmail.com>
2025-07-28 15:24:18 -0700

    feat(Card): create molecule Card

---
+++ src/components/moleculas/card/Card.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+
+import { Card } from './Card';
+
+const Meta = {
+    title: 'Molecule/Card',
+    component: Card,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+}
+
+
+export default Meta;
+type Story = StoryObj<typeof Meta>;
+
+export const Default: Story = {
+    args: {
+        title: 'Card Title',
+        description: 'This is a simple card component that can be used to display content in a structured way.',
+    },
+}
+
+
+
+
