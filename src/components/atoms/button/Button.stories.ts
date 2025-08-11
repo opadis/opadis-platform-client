b96248e0fe149bec0b0012b7d7a7fab44c704c18
Adrián Santos <a23311062@uthermosillo.edu.mx>
2025-08-11 15:30:49 -0700

    fix(accessibility-testing): Fix the details of accessibility testing

--- src/components/atoms/button/Button.stories.ts
+++ src/components/atoms/button/Button.stories.ts
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from "@storybook/react-vite";
 
 import { fn } from "storybook/test";
 
-import { Button } from "./Button";
+import { Button } from "./button";
 
 const Meta = {
   title: "Atoms/Button",
--- src/components/atoms/button/Button.tsx
+++ src/components/atoms/button/Button.tsx
@@ -1,32 +1,28 @@
-import type {ButtonHTMLAttributes} from "react";
+import type { ButtonHTMLAttributes } from "react";
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-    ariaLabel?: string;
-    onClick?: () => void;
-    className?: string;
-    children: React.ReactNode;
+  ariaLabel?: string;
+  onClick?: () => void;
+  className?: string;
+  children: React.ReactNode;
 }
 
-export const Button = ({ 
-    children,
-    className,
-    ariaLabel,
-    onClick, 
-    ...props }: ButtonProps) => {
-    return (
-        <button
-            type="button"
-            className={`btn ${className || ''}`}
-            onClick={onClick}
-            aria-label={ariaLabel || undefined}
-            {...props}
-        >
-            {children}
-        </button>
-    );
-}
-
-
-
-
-
+export const Button = ({
+  children,
+  className,
+  ariaLabel,
+  onClick,
+  ...props
+}: ButtonProps) => {
+  return (
+    <button
+      type="button"
+      className={`btn ${className || ""}`}
+      onClick={onClick}
+      aria-label={ariaLabel || undefined}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
